9bb326a5a1609152d31ed65ec21f0d0677f12d76
matt <MattMacbookPro@Matthews-MacBook-Pro-2.local>
2021-03-25 18:16:31 -0400

    created input validation for sign in page. added protractor for testing

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonService } from '../../service';
-import { FormGroup,FormControl,Validators,FormsModule, } from '@angular/forms';  
+import { FormGroup, FormControl, Validators, FormsModule, FormBuilder } from '@angular/forms'; 
 import { Router } from '@angular/router';
 
 @Component({
@@ -9,7 +9,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+  registerForm: FormGroup;
+  submitted: boolean = false;
   name: String;
   email: String;
   addr: String;
@@ -18,33 +19,41 @@ export class SignupComponent implements OnInit {
   zip: String;
   pass: String;
   conPass: String;
-  States: any=["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA",	"HI",	"ID",	"IL",	"IN",	"IA",	"KS",	"KY",	"LA",	"ME",	"MD",	"MA",	"MI",	"MN",	"MS",	"MO",	"MT",	"NE",	"NV",	"NH",	"NJ",	"NM",	"NY",	"NC",	"ND",	"OH",	"OK",	"OR",	"PA",	"RI",	"SC",	"SD",	"TN",	"TX",	"UT",	"VT",	"VA",	"WA",	"WV",	"WI",	"WY",];
+  states: any=["AL","AK","AZ","AR","CA","CO","CT","DE","FL","GA",	"HI",	"ID",	"IL",	"IN",	"IA",	"KS",	"KY",	"LA",	"ME",	"MD",	"MA",	"MI",	"MN",	"MS",	"MO",	"MT",	"NE",	"NV",	"NH",	"NJ",	"NM",	"NY",	"NC",	"ND",	"OH",	"OK",	"OR",	"PA",	"RI",	"SC",	"SD",	"TN",	"TX",	"UT",	"VT",	"VA",	"WA",	"WV",	"WI",	"WY",];
 
   constructor(private newService :CommonService,
-              private route: Router,) { }
+              private route: Router,
+              private formBuilder: FormBuilder,) { }
 
   ngOnInit(): void {
+
+    this.registerForm = this.formBuilder.group({
+        email: ['', [Validators.required, Validators.email]],
+        name: ['', [Validators.required]],
+        addr: ['', [Validators.required]],
+        city: ['', [Validators.required]],
+        state: ['', [Validators.required]],
+        //need to validate this
+        zip: ['', [Validators.required,Validators.pattern('/[0-9]{5}/')]],
+        conpass: ['', [Validators.required]],
+        pass: ['', [Validators.required,Validators.minLength(6)]]
+    });
   }
 
-  onSave = function(user) {   
-    console.log(user.value);
+  get f() { return this.registerForm.controls; }
+
+  onSave = function() {  
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+        return;
+    } 
+    console.log(this.registerForm.value);
     //if field in user.value fails dont send
-     this.newService.signUp(user.value)  
-     .subscribe(data =>  {  alert(data.data);  
+     this.newService.signUp(this.registerForm.value)  
+     .subscribe(data =>  {  alert(data.data);
 
      }   
      , error => this.errorMessage = error )  
     this.route.navigate(['/notify']);
    }
-}
-
-
-export interface User {
-  name: String;
-  email: String;
-  addr: String;
-  city: String;
-  state: String;
-  zip: String;
-  pass: String;
 }
